c0c18ab733255218dc64f34168920dd595318e81
João Sell <jpjpsell@gmail.com>
2025-09-25 21:14:49 -0300

    adicionando ServerLogger

--- src/com/chatapp/server/ChatServer.java
+++ src/com/chatapp/server/ChatServer.java
@@ -6,18 +6,22 @@ import java.net.Socket;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ChatServer {
-    // Armazena os clientes conectados (nome -> ClientHandler)
     static ConcurrentHashMap<String, ClientHandler> clientes = new ConcurrentHashMap<>();
 
     public static void main(String[] args) {
         int porta = 12345;
+        final ServerLogger logger = new ServerLogger("server_log.txt");
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.logServerShutdown();
+        }));
 
         try (ServerSocket servidor = new ServerSocket(porta)) {
             System.out.println("Servidor iniciado na porta " + porta);
 
             while (true) {
                 Socket socket = servidor.accept();
-                ClientHandler handler = new ClientHandler(socket);
+                ClientHandler handler = new ClientHandler(socket, logger);
                 handler.start();
             }
         } catch (IOException e) {
